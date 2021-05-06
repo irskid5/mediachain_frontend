8ebc99336f98fdd26a597de1d93fb2e9f5fbb953
Vele Tosevski <vele.tosevski@mail.utoronto.ca>
2021-05-06 17:54:34 -0400

    tied to backend

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import Message from './Message';
 import Progress from './Progress';
-// import axios from 'axios';
+import axios from 'axios';
 
 const FileUpload = () => {
   const [file, setFile] = useState('');
@@ -9,46 +9,75 @@ const FileUpload = () => {
   const [filename, setFilename] = useState('Choose File');
   const [uploadedFile, setUploadedFile] = useState({});
   const [message, setMessage] = useState('');
-  const [uploadPercentage] = useState(0); //, setUploadPercentage] = useState(0);
+  const [uploadPercentage, setUploadPercentage] = useState(0);
+
+  function getBase64(file) {
+    return new Promise(resolve => {
+      let fileInfo;
+      let baseURL = "";
+      // Make new FileReader
+      let reader = new FileReader();
+
+      // Convert the file to base64 text
+      reader.readAsDataURL(file);
+
+      // on reader load somthing...
+      reader.onload = () => {
+        // Make a fileInfo Object
+        console.log("Called", reader);
+        baseURL = reader.result.split(",");
+        console.log(baseURL);
+        resolve(baseURL[1]);
+      };
+      console.log(fileInfo);
+    });
+  };
 
   const onChange = e => {
-    setFile(e.target.files[0]);
     setFilename(e.target.files[0].name);
+    getBase64(e.target.files[0])
+      .then(result => {
+        setFile(result);
+        console.log("File Is", file);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   const onSubmit = async e => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('file', file);
+    const formData = {};
+    formData['fileName'] = filename;
+    formData['fileContents'] = file;
 
     setMessage('File uploaded');
 
     try {
-      // const res = await axios.post('/upload', formData, {
-      //   headers: {
-      //     'Content-Type': 'multipart/form-data'
-      //   },
-      //   onUploadProgress: progressEvent => {
-      //     setUploadPercentage(parseInt(Math.round((progressEvent.loaded * 100) / progressEvent.total)));
-      //     setTimeout(() => setUploadPercentage(0), 10000);
-      //   }
-      // });
+      const res = await axios.post('https://1jjs7qjlgj.execute-api.us-east-2.amazonaws.com/dev/upload', formData, {
+        onUploadProgress: progressEvent => {
+          setUploadPercentage(parseInt(Math.round((progressEvent.loaded * 100) / progressEvent.total)));
+          setTimeout(() => setUploadPercentage(0), 10000);
+        }
+      });
 
-      console.log(file);
+      // console.log(file);
       var fileName = filename;
       var filePath = "../test/" + filename;
 
-      // const { fileName, filePath } = res.data;
+      const result = res.data;
+      console.log(res);
       setUploadedFile({ fileName, filePath });
       setLink("https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg")
       console.log(uploadedFile.filePath)
       setMessage('File uploaded');
     } catch (err) {
-      if (err.response.status === 500) {
-        setMessage('There was a problem with the server');
-      } else {
-        setMessage(err.response.data.msg);
-      }
+      // if (err.response.status === 500) {
+      //   setMessage('There was a problem with the server');
+      // } else {
+      //   setMessage(err.response.data.msg);
+      // }
+      console.log(err);
     }
   }
 
